445080383288addc3bda17c54c0e602d5490a0c2
Sheeba193 <bethkerubo193@gmail.com>
2026-03-15 23:40:23 +0300

    more edits on hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/Button";
+import { ArrowRight } from "lucide-react";
 export const Hero = () => {
   return (
     <section className="relative min-h-screen flex items-center overflow-hidden">
       {/* background */}
       <div>
-       <img src="/bg.avif" 
+       <img src="/bgg.jpeg" 
        alt="hero image"
        className="w-full h-full object-cover opacity-40" />
 
@@ -12,7 +14,7 @@ export const Hero = () => {
       </div>
       {/* green dots */}
       <div className="absolute inset-0 overflow-hidden pointer-events-none">
-        {[...Array(30)].map((_, i => (
+        {[...Array(30)].map((_, i )=> (
           <div 
             className="absolute w-1.5 h-1.5 rounded-full opacity-60"
             style={
@@ -25,7 +27,57 @@ export const Hero = () => {
               }
             }
           />
-        )))}
+        ))}
+
+        {/* content */}
+      <div className="container mx-auto px-6 pt-32 pb-20 relative z-10">
+        <div className="grid lg:grid-cols-2 gap-12  items-center">
+          {/* left-column-text content */}
+          <div className="animate-fade-in" >
+            <span className="inline-flex items-center gap-2 px-4 py-2 rounded-full glass text-sm text-primary">
+              <span className="w-2 h-2 bg-primary rounded-full animate-pulse" />
+              Software Engineer * React Specialist 
+            </span>
+            
+          </div>
+
+          {/* headline */}
+          <div className="space-y-4">
+            <h1 className="text-5xl md:text-6xl lg:7xl font-bold leading-tight animate-fade-in animation-delay-100"> 
+              Crafting <span className="text-primary glow-text">Digital </span>
+              <br />
+              experiences with 
+              <br />
+              <span className="font-serif italic font-normal text-white">
+                React.
+              </span>
+            </h1>
+            <p className="text-lg text-muted-foreground max-w-lg animate-fade-in animation-delay-200">
+              I'm a passionate software engineer specializing in React, 
+              dedicated to crafting seamless and engaging web experiences. 
+              With a strong foundation in JavaScript and a keen eye for design, 
+              I bring ideas to life through clean, efficient code. Let's build something amazing together!
+            </p>
+          </div>
+
+            {/* call-to-action buttons */}
+            <div>
+              <Button size="lg">
+                Contact Me <ArrowRight className="w-5 h-5" />
+              </Button>
+              <button>
+                {/* animated svg border */}
+                <svg className="">
+                  <path />
+                </svg>
+              </button>
+            </div>
+      </div>
+
+          
+          {/* right-column-image content */}
+
+        </div>
       </div>
     </section>
 
